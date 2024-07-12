93e8f41631f52bdf0778d6a02e0b26f391317d40
Hakan Fıstık <hakam.fstk@gmail.com>
2024-07-12 14:02:47 +0200

    Add project files.

---
+++ src/andrewlockReadingMode.js
@@ -0,0 +1,23 @@
+removeElement('header');
+removeElement('div.text-sm')
+removeElement('footer.p-2');
+removeElement('footer.p-2');
+removeElement('footer.p-2');
+removeElement('footer.mb-1');
+removeElement('nav.mt-4');
+removeElement('div.gsc-comments');
+removeElement('aside.giscus');
+
+const element = document.querySelector('main');
+element.className = "relative block xl:flex justify-between";
+
+function removeElement(elementName) {
+    try {
+        const element = document.querySelector(elementName);
+        if (element && element.parentElement)
+            element.parentElement.removeChild(element);
+    } catch (ex) {
+        console.log(`Exception when removing element "${elementName}"`);
+        console.log(`Exception : ${ex}`);
+    }
+}
