75cbe7bc74984e1059a54c521e9fffd2ade6558a
Hakan Fıstık <hakam.fstk@gmail.com>
2025-04-16 17:46:00 +0200

    convert it to PDF

--- src/andrewlockReadingMode.js
+++ src/andrewlockReadingMode.js
@@ -10,6 +10,26 @@ removeElement('aside.giscus');
 removeElement('aside.relative.shadow-lg');
 removeElement('div.flex.items-center.p-2.subb-body');
 removeElement("#subbscribe");
+removeElement("header > div");
+removeElement("header > ul");
+
+const mainHead = document.querySelector('header > h1');
+mainHead.style.fontSize = '1.7rem';
+
+const article = document.querySelector('article');
+article.style.padding = '0';
+
+const ps = document.querySelectorAll('p');
+ps.forEach(p => {
+    p.style.marginTop = '0';
+    p.style.marginBottom = '5px';
+});
+
+const prosElements = document.querySelectorAll('.prose');
+prosElements.forEach(element => {
+    element.style.fontSize = '0.8rem';
+    element.style.lineHeight = '1.5';
+});
 
 let headers = document.querySelectorAll('h2');
 headers.forEach(header => {
